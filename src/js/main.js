315dcc483e2ed54ecf6b431c2941097c9bdb92d8
Bryan Dickens <bigbrd1026@gmail.com>
2016-03-31 10:04:21 -0700

    Design spec

--- src/js/main.js
+++ src/js/main.js
@@ -35,3 +35,12 @@ ReactDOM.render(
 // const b = getRandomInt(0, 255);
 // element.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 // document.getElementById('colorSwap').innerHTML = r + ' ' + g + ' ' + b;
+
+// DESIGN
+
+// the correct order of things should be this main.js that has a container that holds the topbar and the mainGame
+  // the topbar has progress info and acct and done button and anything else
+  // mainGame either has a grid of all the sections of pages of games to play (initially just color and profile) on click go to the game
+  // or mainGame has large padding of whitespace and info I for directions(that initially is open) and then in the center is a consistent sized gameGrid with varying amount of options
+    // in gameGrid we have randomNum() of options and then the specific gameDivs (in this case colorDivs) which can be buttons that regen the random number and rerender
+      // each colorDiv has randomColor() or randomAnswer() pulled from DB, etc.  
